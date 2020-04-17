4a6b8a57a6583223c6b26de21d6f118b4efd440c
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-17 09:09:12 -0300

    Update-Bug#14650

--- bianca/14650.spec.js
+++ bianca/14650.spec.js
@@ -3,7 +3,7 @@ describe("Bug#14650", () => {
     it("Login de aluno e núcleo de estágio", () => {
 
         cy.viewport(1366,768);
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+        cy.visit(Cypress.env("nucleo"));
         cy.get("div.banner-content").should("be.visible");
 
     }),
@@ -11,7 +11,7 @@ describe("Bug#14650", () => {
     it("Login empresas", () => {
 
         cy.viewport(1366,768);
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/empresas");
+        cy.visit(Cypress.env("empresa"));
         cy.get("div.banner-content").should("be.visible");
 
     })
