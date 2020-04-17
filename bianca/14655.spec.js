3621904b60f854ff0a3c3c8dc0d0460dd050d318
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-17 09:13:29 -0300

    Update-Bug#14655

--- bianca/14655.spec.js
+++ bianca/14655.spec.js
@@ -3,22 +3,26 @@ describe("Bug#14655", () => {
     it("O campo está desabilitado?", () => {
 
         cy.viewport(1366,768);
-       cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+       cy.visit(Cypress.env("nucleo"));
+       cy.wait(3000)
        cy.get("li:nth-child(1) .perfil-icon").click();
-       cy.get(".access-form > .form-group:nth-child(2) > .form-control").type("nucleo@nucleo.com");
-       cy.get(".form-group:nth-child(3) > .form-control").type("admin");
+       cy.get(".access-form > .form-group:nth-child(2) > .form-control").type(Cypress.env("loginnucleo"));
+       cy.get(".form-group:nth-child(3) > .form-control").type(Cypress.env("senhanucleo"));
+       cy.wait(3000)
        cy.get(".form-group > .btn").click();
 
        cy.wait(3000);
-       cy.get("li:nth-child(6)").click();
+       cy.get("li:nth-child(6)").first().click();
        cy.get(".ng-scope:nth-child(2) > .col-xs-2 > .fa-edit").click();
-       cy.get(".ng-scope:nth-child(2) > .col-sm-3 > .fas").click();
+       cy.get(".fa-bell:nth-child(1)").click()
        cy.get(".checkbox:nth-child(1) > .switch-label").click();
        cy.get("#notificacoes .btn").click();
        cy.get(".btn-success:nth-child(2)").click();
 
+       cy.wait(3000)
        cy.get(".ng-scope:nth-child(2) > .col-xs-2 > .fa-edit").click();
-       cy.get(".ng-scope:nth-child(2) > .col-sm-3 > .fas").click();
+
+       cy.get(".fa-bell:nth-child(1)")
        cy.get("input[id='notificarAluno']").should("be.disabled");
 
 
