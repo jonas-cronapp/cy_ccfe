65a5d912001ba9dbb55ef70c22c8610d48a23943
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-17 11:55:30 -0300

    Update-Bug#14618

--- Filipe/14618.spec.js
+++ Filipe/14618.spec.js
@@ -1,13 +1,13 @@
 describe ("Bug 14618", function(){
     it ("Confirmação bug",function(){
-        cy.visit ("https://empregabilidade-ui.cronapp.io/#/empresas")
-        cy.get ("input[placeholder='E-mail ou CNPJ']").type ("21333333444422")
-        cy.get ("input[placeholder='Senha']") .type ("21333")
+        cy.visit (Cypress.env('empresa'))
+        cy.get ("input[placeholder='E-mail ou CNPJ']").type (Cypress.env('loginempresa'))
+        cy.get ("input[placeholder='Senha']") .type (Cypress.env('senhaempresa'))
         cy.get (".btn").first().click()
         cy.wait (2000)
-        cy.visit ("https://empregabilidade-ui.cronapp.io/#/empresas/cadastro")
+        cy.get("li[ui-sref='empresa.cadastro']").click()
         cy.wait (4000)
-        cy.get (".bloco-cadastro:nth-child(2) .fa-plus-square").click()
+        cy.get (".bloco-cadastro:nth-child(3) .fa-plus-square").last().click()
         cy.get(".modal-footer > .btn-success").click()
         cy.wait (2000)
         cy.screenshot()
