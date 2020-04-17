61de61f59b587e04e2610c2da88825cda97a5ddf
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-17 09:07:40 -0300

    Update-Bug#14630

--- bianca/14630.spec.js
+++ bianca/14630.spec.js
@@ -3,10 +3,10 @@ describe("Bug#14630", () => {
     it("Campo numero da apolice aceita somente número?", () => {
 
         cy.viewport(1366,768);
-       cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+       cy.visit(Cypress.env('nucleo'));
        cy.get("li:nth-child(1) .perfil-icon").click();
-       cy.get(".access-form > .form-group:nth-child(2) > .form-control").type("nucleo@nucleo.com");
-       cy.get(".form-group:nth-child(3) > .form-control").type("admin");
+       cy.get(".access-form > .form-group:nth-child(2) > .form-control").type(Cypress.env("loginnucleo"));
+       cy.get(".form-group:nth-child(3) > .form-control").type(Cypress.env("senhanucleo"));
        cy.get(".form-group > .btn").click();
 
        cy.get(".sidebar-links > li:nth-child(4)").click();
