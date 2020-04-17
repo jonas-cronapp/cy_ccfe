a59510a567b7b548896fdcc48386ef3e000ea3b0
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-17 09:12:22 -0300

    Update-Bug#14653

--- bianca/14653.spec.js
+++ bianca/14653.spec.js
@@ -3,14 +3,14 @@ describe("Bug#14653", () => {
     it("Checkbox 1 está desabilitado?", () => {
 
         cy.viewport(1366,768);
-       cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+       cy.visit(Cypress.env("nucleo"));
        cy.get("li:nth-child(1) .perfil-icon").click();
-       cy.get(".access-form > .form-group:nth-child(2) > .form-control").type("nucleo@nucleo.com");
-       cy.get(".form-group:nth-child(3) > .form-control").type("admin");
+       cy.get(".access-form > .form-group:nth-child(2) > .form-control").type(Cypress.env("loginnucleo"));
+       cy.get(".form-group:nth-child(3) > .form-control").type(Cypress.env("senhanucleo"));
        cy.get(".form-group > .btn").click();
 
        cy.wait(3000);
-       cy.get("li:nth-child(6)").click();
+       cy.get("li:nth-child(6)").first().click();
        cy.get(".ng-scope:nth-child(2) > .col-xs-2 > .fa-edit").click();
        cy.wait(4000);
        cy.get("tr:nth-child(1) > .col-md-6:nth-child(1) .switch-label").click();
@@ -24,14 +24,14 @@ describe("Bug#14653", () => {
     it("Checkbox 2 está desabilitado?", () => {
 
         cy.viewport(1366,768);
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+        cy.visit(Cypress.env("nucleo"));
         cy.get("li:nth-child(1) .perfil-icon").click();
-        cy.get(".access-form > .form-group:nth-child(2) > .form-control").type("nucleo@nucleo.com");
-        cy.get(".form-group:nth-child(3) > .form-control").type("admin");
+        cy.get(".access-form > .form-group:nth-child(2) > .form-control").type(Cypress.env("loginnucleo"));
+        cy.get(".form-group:nth-child(3) > .form-control").type(Cypress.env("senhanucleo"));
         cy.get(".form-group > .btn").click();
  
         cy.wait(3000);
-        cy.get("li:nth-child(6)").click();
+        cy.get("li:nth-child(6)").first().click();
         cy.get(".ng-scope:nth-child(2) > .col-xs-2 > .fa-edit").click();
         cy.wait(4000);
         cy.get(".col-md-6:nth-child(2) .switch-label").click();
@@ -45,14 +45,14 @@ describe("Bug#14653", () => {
     it("Checkbox 3 está desabilitado?", () => {
 
         cy.viewport(1366,768);
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+        cy.visit(Cypress.env("nucleo"));
         cy.get("li:nth-child(1) .perfil-icon").click();
-        cy.get(".access-form > .form-group:nth-child(2) > .form-control").type("nucleo@nucleo.com");
-        cy.get(".form-group:nth-child(3) > .form-control").type("admin");
+        cy.get(".access-form > .form-group:nth-child(2) > .form-control").type(Cypress.env("loginnucleo"));
+        cy.get(".form-group:nth-child(3) > .form-control").type(Cypress.env("senhanucleo"));
         cy.get(".form-group > .btn").click();
  
         cy.wait(3000);
-        cy.get("li:nth-child(6)").click();
+        cy.get("li:nth-child(6)").first().click();
         cy.get(".ng-scope:nth-child(2) > .col-xs-2 > .fa-edit").click();
         cy.wait(4000);
         cy.get("tr:nth-child(2) .switch-label").click();
