5c74f70f10b3c8e38dafa1504c028bf6a0eba165
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-17 09:11:48 -0300

    Update-Bug#14651

--- bianca/14651.spec.js
+++ bianca/14651.spec.js
@@ -3,16 +3,17 @@ describe("Bug#14651", () => {
     it("A div está responsiva?", () => {
 
         cy.viewport(1366,768);
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/instituicoes/unicuritiba");
+        cy.visit(Cypress.env("nucleo"));
 
 
         cy.get("li:nth-child(1) .perfil-icon").click()
-
-        cy.get("input[ng-model='perfil.login']").type("nucleo@nucleo.com")
-        cy.get("input[ng-model='perfil.senha']").type("admin")
+        cy.wait(3000)
+        cy.get("input[ng-model='perfil.login']").type(Cypress.env("loginnucleo"))
+        cy.get("input[ng-model='perfil.senha']").type(Cypress.env("senhanucleo"))
         cy.get("button[ng-click='validarLogin(perfil)']").click();
 
-       cy.get("li:nth-child(6)").click()
+       cy.wait(3000)
+       cy.get("li:nth-child(6)").first().click()
        cy.get("tr.ng-scope > td:nth-child(5)").last().then(($unidade) =>{
 
 
