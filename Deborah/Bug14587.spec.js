611989ef60adf09a651bfbdd037748d40e5da0c9
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-20 11:03:04 -0300

    Update-Bug#14587

--- Deborah/Bug14587.spec.js
+++ Deborah/Bug14587.spec.js
@@ -1,17 +1,17 @@
 describe ("Empregabilidade", function() {
 	it("Automatização do Bug 14587", function () {
-		cy.visit("https://empregabilidade-ui.cronapp.io/#/empresas");
+		cy.visit(Cypress.env('empresa'));
 		cy.wait(1500);
 		cy.viewport(1440,900);
-		cy.get("input[placeholder='E-mail ou CNPJ']").type("21333333444422");
-		cy.get("input[placeholder='Senha']").type("21333");
+		cy.get("input[placeholder='E-mail ou CNPJ']").type(Cypress.env('loginempresa'));
+		cy.get("input[placeholder='Senha']").type(Cypress.env('senhaempresa'));
 		cy.get("button[ng-click='validarLogin(perfil)']").click();
 		cy.wait(2000);
 		/*cy.get("body > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div > div:nth-child(4) > div:nth-child(3) > div:nth-child(2) > ul > li:nth-child(1) > div:nth-child(1) > div:nth-child(2) > i:nth-child(3)").then(
 			$x => {$x.css('border', '2px solid red')}
 
 			).click().then($y => {$y.css('border', 'none')});*/
-		cy.get("i[ng-click='openCard($event)']").first().then(
+		cy.get("i[ng-click='openCard($event)']").last().then(
 				$x => {$x.css('border', '2px solid red')}
 			).click().then(
 				$y => {$y.css('border', 'none')}
@@ -22,7 +22,7 @@ describe ("Empregabilidade", function() {
 			
 
 			).click();*/
-		cy.get("span[class='vaga-info-nome ng-binding']").first().then(
+		cy.get("span[class='vaga-info-nome ng-binding']").last().then(
 				$z => {$z.css('border', '2px solid red')}
 			).click();
 		cy.wait(1500);
