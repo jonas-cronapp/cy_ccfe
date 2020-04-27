7d61ab0021eec08ba317f5ec95a465790b6f4fa0
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-27 11:51:57 -0300

    Update-Bug#14633

--- bianca/14633.spec.js
+++ bianca/14633.spec.js
@@ -2,25 +2,24 @@ describe("Bug#14633", () => {
 
     it("A div está centralizada?", () => {
 
+        cy.visit(Cypress.env("empresa"));
         cy.viewport(768,1024);
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/empresas");
 
 
        // cy.get("li:nth-child(1) .perfil-icon").click()
 
-        cy.get("input[ng-model='perfil.login']").type("21333333444422")
-        cy.get("input[ng-model='perfil.senha']").type("21333")
+        cy.get("input[ng-model='perfil.login']").type("pedro.lima@cronapp.io")
+        cy.get("input[ng-model='perfil.senha']").type("123456789")
         cy.get("button[ng-click='validarLogin(perfil)']").click();
 
-       //cy.get("li:nth-child(5)").click()
-       cy.get("button.menu-btn").click()
-       cy.get("li:nth-child(7)").click()
+        cy.wait(3000);
+        cy.get("button.menu-btn").click()
+        cy.get("li:nth-child(7)").click()
 
-       cy.get(".col-lg-12:nth-child(1)").should("have.class","col-sm-12").and("have.class","col-md-12")
+        cy.get(".col-lg-12:nth-child(1)").should("have.class","col-sm-12").and("have.class","col-md-12")
        
        
-     
-       
+    
 
     })
 
