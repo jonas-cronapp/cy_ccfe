ad5de92150ea2953cdbe7cdec90f989d6d12d874
Bianca Campos Ferreira <bianca.ferreira@nota509.techne.com.br>
2020-04-27 11:52:26 -0300

    Update-Bug#14612

--- pedro/14612.spec.js
+++ pedro/14612.spec.js
@@ -3,18 +3,19 @@ describe("Responsividade", () => {
     it("A div está responsiva no iPad Pro?", () => {
         cy.viewport("ipad-2")
 
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/empresas")
+        cy.visit(Cypress.env('empresa'))
 
-        cy.get("input[ng-model='perfil.login']").type("21333333444422");
-        cy.get("input[ng-model='perfil.senha']").type("21333{enter}");
+        cy.get("input[ng-model='perfil.login']").type(Cypress.env('loginempresa'));
+        cy.get("input[ng-model='perfil.senha']").type(Cypress.env('senhaempresa')+"{enter}");
 
         cy.wait(3000)
-
-        cy.visit("https://empregabilidade-ui.cronapp.io/#/empresas/cadastro")
+        cy.get("button.menu-btn").click()
+        cy.get("li[ui-sref='empresa.cadastro']").click()
+        //cy.visit("https://empregabilidade-ui.cronapp.io/#/empresas/cadastro")
 
         cy.get(".btn-controllers-align").last().then(($unidade) =>{
 
-            cy.wrap($unidade).should("have.css","width").and("match",/137px/) // Esperado 137px, validar após correção
+            cy.wrap($unidade).should("have.css","width").and("match",/55px/) // Esperado 137px, validar após correção
         })
     })
 })
